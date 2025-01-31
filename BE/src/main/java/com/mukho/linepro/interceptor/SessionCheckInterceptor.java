4a046fd2519b2573545be156f2b0c0fc22c0f985
Mukho <mukho@kakao.com>
2025-02-01 03:56:15 +0900

    Feat: 세션 새로고침 기능 추가

--- BE/src/main/java/com/mukho/linepro/interceptor/SessionCheckInterceptor.java
+++ BE/src/main/java/com/mukho/linepro/interceptor/SessionCheckInterceptor.java
@@ -1,12 +1,14 @@
 package com.mukho.linepro.interceptor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mukho.linepro.util.SessionUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -37,5 +39,10 @@ public class SessionCheckInterceptor implements HandlerInterceptor {
         }
 
         return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+        SessionUtil.refreshSession(request);
     }
 }
